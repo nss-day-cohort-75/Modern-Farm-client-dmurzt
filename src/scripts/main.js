78b1675711cd87663bb96e2f852bc67678cee5e2
gregor-J8 <gregor.johnson@outlook.com>
2025-01-23 14:36:11 -0600

    merging main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,16 @@
 import { createPlan } from "./plan.js"
-import { addPlant } from "./field.js"
+import { addPlant, usePlants } from "./field.js"
+import { catalog } from "./catalog.js"
+import { plantSeeds } from "./tractor.js"
 
 console.log("Welcome to the main module")
 
 const plan = createPlan()
+
+const tractor = plantSeeds(plan)
+const field = addPlant(tractor)
+const useablePlants = usePlants(field)
+
+ const placeHTML = document.querySelector(".container")
+ const listHTML = catalog(useablePlants)
+placeHTML.innerHTML = listHTML
